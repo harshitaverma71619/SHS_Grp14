d9f9875fd022438999e15b46d9cba6e44e778e85
Harshil Gandhi <harshil.gandhi69@gmail.com>
2022-03-15 20:45:58 -0700

    labstaff models.py changes

--- LabStaff/models.py
+++ LabStaff/models.py
@@ -1,3 +1,4 @@
+from tkinter import N
 from django.db import models
 
 # Create your models here.
@@ -5,6 +6,7 @@ class LabReports(models.Model):
     report_id = models.IntegerField(null=False)
     report_info = models.CharField(max_length=1000, null=False)
     doctor_id = models.IntegerField(null=False)
-    patient_id = models.IntegerField(null=False)
+    patient_id = models.IntegerField(null=False) 
     lab_staff_id = models.IntegerField(null=False)
     report_status = models.CharField(max_length=100, null=False)
+    request_status = models.CharField(max_length=100, null=False, default="Pending")
